2890fd43fbd2dd215af09d8354ebb25c45e6b9a0
daydreamboy <wesley4chen@gmail.com>
2018-05-20 22:51:45 +0800

    add code

---
+++ 01_helloworld/test.js
@@ -0,0 +1,3 @@
+const _ = require('underscore');
+console.log(_.range(5));
+console.log('hello, world!');
---
+++ 02_mkdirp/mkdir.js
@@ -0,0 +1,9 @@
+const mkdirp = require('mkdirp');
+mkdirp('foo', function (err) {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log('Directory created!');
+    }
+});
