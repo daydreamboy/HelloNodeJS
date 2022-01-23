abc16b255bbaa0abb4feafcdfbf1b8d16f56d047
daydreamboy <wesley4chen@gmail.com>
2022-01-23 20:48:10 +0800

    18_icesjs: add expression.m4

--- 18_icesjs/OCPWorkbench/src/parsers/ocs_unit_test/generate_pegjs.sh
+++ 18_icesjs/OCPWorkbench/src/parsers/ocs_unit_test/generate_pegjs.sh
@@ -5,4 +5,5 @@ set -e
 m4 type_encoding.m4 > type_encoding.pegjs
 m4 literal_type.m4 > literal_type.pegjs
 m4 block_parameter_list.m4 > block_parameter_list.pegjs
-m4 expression_list.m4 > expression_list.pegjs
+m4 expression.m4 > expression.pegjs
+m4 expression_list_or_tuple.m4 > expression_list_or_tuple.pegjs
